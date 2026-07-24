573dbb7bb4c2a4d5d6b3acc2d23519f89f5c13e0
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-07-24 23:58:46 +0800

    Starting movement

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,15 +1,23 @@
 extends EntityClass
 class_name Player
 
-var desire_scale : Vector2 = Vector2(1.5,1.5)
+@onready var sprite: Sprite2D = %sprite
+
+var sprite_desire_scale : Vector2 = Vector2(2,2)
+var desire_move : Vector2
 
 func _init() -> void:
 	EventBus.MusicBeatTick.connect(beat_tick)
 
 func beat_tick() -> void:
-	scale.x += 0.4
+	sprite.scale.x += 0.4
 	%thif.play()
+	
+	global_position += desire_move * 32
 
 func _physics_process(delta: float) -> void:
-	scale.y = scale.x
-	scale = lerp(scale, desire_scale, 20.0 * delta)
+	sprite.scale.y = sprite.scale.x
+	sprite.scale = lerp(sprite.scale, sprite_desire_scale, 20.0 * delta)
+
+func _input(event: InputEvent) -> void:
+	desire_move = Input.get_vector("left", "right", "up", "down")
